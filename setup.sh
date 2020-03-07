03ec880869397b702cf0b28c8578c05580542ad1
gabrielmv <gabriel.meneses@live.com>
2020-03-06 21:38:28 -0300

    paths

--- setup.sh
+++ setup.sh
@@ -28,3 +28,76 @@ PUID=`(id -u $USER)`
 PGID=`(id -g $USER)`
 
 docker-compose up
+
+
+# zfs
+sudo apt install zfsutils-linux -y
+sudo zpool create yosemite /dev/sdb
+
+#docker
+
+sudo chown -R gabriel:gabriel /yosemite
+sudo chmod -R 777 /yosemite
+
+docker create \
+  --name=jackett \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/Sao_Paulo \
+  -p 9117:9117 \
+  -v ~/config:/config \
+  -v /yosemite/downloads:/downloads \
+  --restart unless-stopped \
+  linuxserver/jackett
+
+docker create \
+  --name=deluge \
+  --net=host \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/Sao_Paulo \
+  -e UMASK_SET=022 `#optional` \
+  -e DELUGE_LOGLEVEL=error `#optional` \
+  -v ~/config:/config \
+  -v /yosemite/downloads:/downloads \
+  --restart unless-stopped \
+  linuxserver/deluge
+
+docker run \
+  --name=sonarr \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/Sao_Paulo \
+  -e UMASK_SET=022 `#optional` \
+  -p 8989:8989 \
+  -v ~/config:/config \
+  -v /yosemite/tvseries:/tv \
+  -v /yosemite/downloads:/downloads \
+  --restart unless-stopped \
+  linuxserver/sonarr
+
+docker create \
+  --name=radarr \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e TZ=America/Sao_Paulo \
+  -e UMASK_SET=022 `#optional` \
+  -p 7878:7878 \
+  -v ~/config:/config \
+  -v /yosemite/movies:/movies \
+  -v /yosemite/downloads:/downloads \
+  --restart unless-stopped \
+  linuxserver/radarr
+
+docker create \
+  --name=plex \
+  --net=host \
+  -e PUID=1000 \
+  -e PGID=1000 \
+  -e VERSION=docker \
+  -e UMASK_SET=022 `#optional` \
+  -v ~/config:/config \
+  -v /yosemite/tvseries:/tv \
+  -v /yosemite/movies:/movies \
+  --restart unless-stopped \
+  linuxserver/plex
